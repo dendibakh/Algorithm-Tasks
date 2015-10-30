78d52f5e7c5603a8a5773a5033ef8712b761df96
Denis <dendibakh@gmail.com>
2015-10-30 15:33:46 +0100

    Added implementation itself

--- Coursera class/SimpleDataStructures/Heap/src/heap.h
+++ Coursera class/SimpleDataStructures/Heap/src/heap.h
@@ -1,16 +1,102 @@
 #pragma once
 #include <vector>
 #include <limits>
+#include <iostream>
+#include <stdexcept>
 
 template<class T>
 class Heap
 {
+private:
+    void bubbleUp(size_t index)
+    {
+        size_t parent = 0;
+        while (hasParent(index))
+        {
+            parent = getParent(index);
+            if (array[index] < array[parent]) 
+                std::swap(array[index], array[parent]);
+            index = parent;
+        }
+    }
+
+    void bubbleDown(size_t index)
+    {
+        size_t left = getLeftChild(index);
+        size_t right = getRightChild(index);
+        size_t arSize = array.size();
+        size_t indexToSwap = 0;
+
+        while ( left < arSize || right < arSize ) // there are childrens
+        {
+            if (left < arSize && right < arSize)
+            {
+                if (array[left] < array[right])
+                    indexToSwap = left;
+                else
+                    indexToSwap = right;
+            }
+            else if (left < arSize)
+            {
+                indexToSwap = left;
+            }
+            else if (right < arSize)
+            {
+                indexToSwap = right;
+            }
+
+            if ( array[index] <= array[indexToSwap] ) // we are done
+                break;
+
+            std::swap(array[index], array[indexToSwap]);
+
+            index = indexToSwap;
+            left = getLeftChild(index);
+            right = getRightChild(index);
+        }
+    }
+
+    void PrintArray()
+    {
+        for (size_t i = 0; i < array.size(); ++i )
+        {
+            std::cout << array[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+
 public:
-    Heap() {}
+    Heap() 
+    {
+    }
+
+    template <class U>
+    Heap(const U& source) 
+    {
+        array.insert(array.begin(), source.begin(), source.end());
+        size_t N = array.size();
+        for (size_t i = 0; i < N; ++i )
+        {
+            bubbleDown(N - i - 1);
+        }
+    }
+    
     void insert(const T& val)
     {
         array.push_back(val);
+        bubbleUp(array.size() - 1);
     } 
+
+    T deleteMin()
+    {
+        if (array.empty())
+            throw std::runtime_error("You are trying to extract from empty heap!");
+        T val = array.front();
+        std::swap(array.back(), array.front());
+        array.pop_back();
+        bubbleDown(0);
+        return val;
+    }
 
     static size_t getLeftChild(size_t index)
     {
--- Coursera class/SimpleDataStructures/Heap/test/heapUnitTest.cpp
+++ Coursera class/SimpleDataStructures/Heap/test/heapUnitTest.cpp
@@ -7,6 +7,9 @@ using namespace testing;
 class FakeIntHeap : public Heap<int>
 {
 public:
+    FakeIntHeap() : Heap<int>() {}
+    template <class U>
+    FakeIntHeap(const U& source) : Heap<int>(source) { }
     using Heap<int>::array;
 };
 
@@ -66,7 +69,7 @@ TEST(HeapUnitTest, 7)
      FakeIntHeap heap;
      heap.insert(2);
      heap.insert(1);
-     //checkTwoVectorsAreEqual({1, 2}, heap.array);
+     checkTwoVectorsAreEqual({1, 2}, heap.array);
 }
 
 TEST(HeapUnitTest, 8)
@@ -120,4 +123,168 @@ TEST(HeapUnitTest, 11)
     }
 
     ASSERT_EQ(2, counter);
+}
+
+TEST(HeapUnitTest, 12)
+{
+    FakeIntHeap heap;
+    heap.insert(2);
+    heap.insert(1);
+    heap.insert(0);
+    checkTwoVectorsAreEqual({0, 2, 1}, heap.array);
+}
+
+TEST(HeapUnitTest, 13)
+{
+    // Skiena example
+    FakeIntHeap heap;
+    heap.insert(1804);
+    heap.insert(1776);
+    heap.insert(1492);
+    heap.insert(2001);
+    heap.insert(1963);
+    heap.insert(1918);
+    heap.insert(1783);
+    heap.insert(1945);
+    heap.insert(1865);
+    heap.insert(1941);
+    checkTwoVectorsAreEqual({1492, 1804, 1776, 1865, 1941, 1918, 1783, 2001, 1945, 1963}, heap.array);
+}
+
+TEST(HeapUnitTest, 14)
+{
+    // Skiena example with duplicates
+    FakeIntHeap heap;
+    heap.insert(1804);
+    heap.insert(1776);
+    heap.insert(1492);
+    heap.insert(2001);
+    heap.insert(1963);
+    heap.insert(1918);
+    heap.insert(1783);
+    heap.insert(1945);
+    heap.insert(1865);
+    heap.insert(1941);
+    heap.insert(1941);
+    heap.insert(1941);
+    heap.insert(1941);
+    checkTwoVectorsAreEqual({1492, 1804, 1776, 1865, 1941, 1918, 1783, 2001, 1945, 1963, 1941, 1941, 1941}, heap.array);
+}
+
+TEST(HeapUnitTest, 15)
+{
+    // Skiena example
+    FakeIntHeap heap;
+    heap.insert(1804);
+    heap.insert(1776);
+    heap.insert(1492);
+    heap.insert(2001);
+    heap.insert(1963);
+    heap.insert(1918);
+    heap.insert(1783);
+    heap.insert(1945);
+    heap.insert(1865);
+    heap.insert(1941);
+    checkTwoVectorsAreEqual({1492, 1804, 1776, 1865, 1941, 1918, 1783, 2001, 1945, 1963}, heap.array);
+    ASSERT_EQ(1492, heap.deleteMin());
+}
+
+TEST(HeapUnitTest, 16)
+{
+     FakeIntHeap heap;
+     heap.insert(2);
+     heap.insert(1);
+     checkTwoVectorsAreEqual({1, 2}, heap.array);
+     ASSERT_EQ(1, heap.deleteMin());
+     checkTwoVectorsAreEqual({2}, heap.array);
+     ASSERT_EQ(2, heap.deleteMin());
+     checkTwoVectorsAreEqual({}, heap.array);
+}
+
+TEST(HeapUnitTest, 17)
+{
+     FakeIntHeap heap;
+     heap.insert(2);
+     heap.insert(1);
+     heap.insert(0);
+     checkTwoVectorsAreEqual({0, 2, 1}, heap.array);
+     ASSERT_EQ(0, heap.deleteMin());
+     checkTwoVectorsAreEqual({1, 2}, heap.array);
+     ASSERT_EQ(1, heap.deleteMin());
+     checkTwoVectorsAreEqual({2}, heap.array);
+     ASSERT_EQ(2, heap.deleteMin());
+     checkTwoVectorsAreEqual({}, heap.array);
+}
+
+TEST(HeapUnitTest, 18)
+{
+    // Skiena example
+    FakeIntHeap heap;
+    heap.insert(1804);
+    heap.insert(1776);
+    heap.insert(1492);
+    heap.insert(2001);
+    heap.insert(1963);
+    heap.insert(1918);
+    heap.insert(1783);
+    heap.insert(1945);
+    heap.insert(1865);
+    heap.insert(1941);
+    checkTwoVectorsAreEqual({1492, 1804, 1776, 1865, 1941, 1918, 1783, 2001, 1945, 1963}, heap.array);
+    ASSERT_EQ(1492, heap.deleteMin());
+    ASSERT_EQ(1776, heap.deleteMin());
+    ASSERT_EQ(1783, heap.deleteMin());
+    ASSERT_EQ(1804, heap.deleteMin());
+    ASSERT_EQ(1865, heap.deleteMin());
+    ASSERT_EQ(1918, heap.deleteMin());
+    ASSERT_EQ(1941, heap.deleteMin());
+    ASSERT_EQ(1945, heap.deleteMin());
+    ASSERT_EQ(1963, heap.deleteMin());
+    ASSERT_EQ(2001, heap.deleteMin());
+}
+
+TEST(HeapUnitTest, 19)
+{
+    std::vector<int> source = { 1804, 1776, 1492, 2001, 1963, 1918, 1783 };
+    FakeIntHeap heap(source);
+    
+    checkTwoVectorsAreEqual({1492, 1776, 1783, 2001, 1963, 1918, 1804}, heap.array);
+    ASSERT_EQ(1492, heap.deleteMin());
+    ASSERT_EQ(1776, heap.deleteMin());
+    ASSERT_EQ(1783, heap.deleteMin());
+    ASSERT_EQ(1804, heap.deleteMin());
+    ASSERT_EQ(1918, heap.deleteMin());
+    ASSERT_EQ(1963, heap.deleteMin());
+    ASSERT_EQ(2001, heap.deleteMin());
+}
+
+TEST(HeapUnitTest, 20)
+{
+    std::vector<int> source = { 1804, 1776, 1492, 2001, 1963, 1918, 1783, 1945, 1865, 1941 };
+    FakeIntHeap heap(source);
+
+    std::vector<int> expectedResult = { 1492, 1776, 1783, 1804, 1865, 1918, 1941, 1945, 1963, 2001 };
+    
+    std::vector<int> actualResult;
+    while (!heap.array.empty())
+    {
+       actualResult.push_back(heap.deleteMin());
+    }
+
+    checkTwoVectorsAreEqual(expectedResult, actualResult);
+}
+
+TEST(HeapUnitTest, 21)
+{
+    std::vector<int> source = { };
+    FakeIntHeap heap(source);
+
+    try
+    {
+       heap.deleteMin();
+       FAIL() << "There must be an exception";
+    }
+    catch(std::exception& e)
+    {
+    }
 }
